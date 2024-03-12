a0b3474e395c9ec2d62c7911470b013ac3c6005e
Mi4-04 <zakharovm2001@mail.ru>
2024-03-12 11:40:24 +0300

    add missing provider JWTStratagy for AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,25 @@
-import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/entities/user.entity';
-import AuthResolver from './auth.resolver';
-import SignInService from './services/sign-in';
-import SignUpService from './services/sign-up';
+import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from 'src/entities/user.entity'
+import AuthResolver from './auth.resolver'
+import { JwtStratagy } from './jwt-stratagy'
+import SignInService from './services/sign-in'
+import SignUpService from './services/sign-up'
 
-const configService = new ConfigService();
+const configService = new ConfigService()
 
 @Module({
   imports: [
     JwtModule.register({
       secret: configService.get('JWT_SECRET'),
-      signOptions: { expiresIn: '7 days' },
+      signOptions: { expiresIn: '7 days' }
     }),
     PassportModule,
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User])
   ],
-  providers: [SignUpService, SignInService, AuthResolver],
+  providers: [SignUpService, SignInService, AuthResolver, JwtStratagy]
 })
 export class AuthModule {}
